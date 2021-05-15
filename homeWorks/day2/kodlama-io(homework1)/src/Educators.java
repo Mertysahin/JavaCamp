b55410be56a5d57217b77435bcddebe972a3b8f0
Mertcan <merty1304@gmail.com>
2021-05-15 17:06:31 +0300

    kamp ödevleri

---
+++ homeWorks/day2/kodlama-io(homework1)/src/Educators.java
@@ -0,0 +1,17 @@
+
+public class Educators 
+{
+	public Educators(int educatorId, String educatorName, int educatorYas)
+	{
+		this.educatorId=educatorId;
+		this.educatorName=educatorName;
+		this.educatorYas=educatorYas;
+	}
+	
+	
+	int educatorId;
+	String educatorName;
+	int educatorYas;
+	
+	
+}
